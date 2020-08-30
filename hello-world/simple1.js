63943e72a61ec3f37c0d555ec855a7580f2e128e
dev-do <dev-do@naver.com>
2020-08-30 12:09:24 +0900

    변환 및 주석처리 코드 삭제

--- hello-world/simple1.js
+++ hello-world/simple1.js
@@ -1,56 +1,23 @@
 function LikeButton() {
   const [liked, setLiked] = React.useState(false);
   const text = liked ? "좋아요 취소" : "좋아요";
-  return React.createElement(
-    "button",
-    { onClick: () => setLiked(!liked) },
-    text
-  );
+  return <button onClick={() => setLiked(!liked)}>{text}</button>;
 }
-
-// ReactDOM.render(
-//   React.createElement(
-//     "div",
-//     null,
-//     React.createElement(LikeButton),
-//     React.createElement(LikeButton),
-//     React.createElement(LikeButton)
-//   ),
-//   domContainer
-// );
 
 function Container() {
   const [count, setCount] = React.useState(0);
-  return React.createElement(
-    "div",
-    null,
-    React.createElement(LikeButton),
-    React.createElement(
-      "div",
-      { style: { marginTop: 20 } },
-      React.createElement("span", null, "현재 카운트: "),
-      React.createElement("span", { style: { marginRight: 10 } }, count),
-      React.createElement(
-        "button",
-        { onClick: () => setCount(count + 1) },
-        "증가"
-      ),
-      React.createElement(
-        "button",
-        { onClick: () => setCount(count - 1) },
-        "감소"
-      )
-    )
+  return (
+    <div>
+      <LikeButton />
+      <div>
+        <span>현재 카운트: </span>
+        <span style={{ marginRight: 10 }}>{count}</span>
+        <button onClick={() => setCount(count + 1)}>증가</button>
+        <button onClick={() => setCount(count - 1)}>감소</button>
+      </div>
+    </div>
   );
 }
 
 const domContainer = document.getElementById("root");
 ReactDOM.render(React.createElement(Container), domContainer);
-
-/* React.createElement(
-  "div",
-  null,
-  React.createElement("p", null, "hello"),
-  React.createElement("p", null, "world")
-);
- */
